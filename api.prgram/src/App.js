d900edac91024ca264973f33fd6e99ae1f0140b7
luna00627 <az991130627@gmail.com>
2023-12-19 18:07:00 +0800

    app

---
+++ api.prgram/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Axios from'axios';
+import React from 'react';
+import {useEffect, useState} from'react'
+
+function App() {
+  const [catFact, setCatFact] = useState("")
+  useEffect(()=>{
+    Axios.get("https://catfact.ninja/fact").then((res)=>{
+      console.log(res.data);
+      setCatFact(res.data.fact);
+      });
+    },[]);
+  return(
+    <div className='App'>
+      {catFact}
+    </div>
+  );
+}
+
+export default App;
